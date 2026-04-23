c4173e72528a342f0c520cca24dd53d0907dc552
LINYANGSHENG <153968692+LinYsssss@users.noreply.github.com>
2026-04-23 10:26:05 -0700

    Add rechargeCard function to handle card recharges

--- src/api/card.js
+++ src/api/card.js
@@ -10,6 +10,11 @@ export function getCardRecordPage(params) {
   return request.get('/svc/card/record/page', { params })
 }
 
+// 充值
+export function rechargeCard(amount) {
+  return request.post('/svc/card/recharge', null, { params: { amount } })
+}
+
 // 挂失
 export function reportLoss(cardNo) {
   return request.post('/svc/card/loss/report', null, { params: { cardNo } })
